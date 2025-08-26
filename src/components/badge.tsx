c67962f93049b3c4aac73ba5a93b80bdc5b32d74
Lucas Guedes <lucas.gkrt@gmail.com>
2025-08-26 17:12:43 -0300

    feat: Adiciona o componente Badge para tags de status
    Cria um componente `Badge` reutilizável para a aplicação, utilizando a biblioteca `class-variance-authority` (cva).
    
    - O componente oferece variantes de cores (`primary` e `secondary`) e tamanhos (`sm`) para flexibilidade.
    - Define variantes de estilo para o texto e para o contêiner do badge separadamente para um controle mais granular.
    - Importa o componente `Text` para manter a consistência na tipografia.
    - Usa a biblioteca `cva` para simplificar a gestão de classes CSS e tipagem, resultando em um código mais limpo e legível.

---
+++ src/components/badge.tsx
@@ -0,0 +1,50 @@
+import { cva, type VariantProps } from 'class-variance-authority';
+import Text from './text';
+
+export const badgeVariants = cva("inline-flex items-center justify-center rounded-full",{
+    variants: {
+        variant:{
+            primary: "bg-green-light",
+            secundary: "bg-pink-light",
+        },
+        size:{
+            sm: "py-0.5 px-2"
+        },
+    },
+    defaultVariants:{
+        variant: "primary",
+        size: "sm"
+    }
+});
+
+export const badgeTextVariants = cva("",{
+    variants:{
+        variant:{
+            primary: "text-green-dark",
+            secundary: "text-pink-dark"
+        }
+    },
+    defaultVariants:{
+        variant: "primary"
+    }
+})
+
+interface BadgeProps extends React.ComponentProps<"div">, VariantProps<typeof badgeVariants>{}
+
+export default function Badge({
+    variant,
+    size,
+    className,
+    children,
+    ...props
+
+}: BadgeProps) {
+
+    return (
+        <div className={badgeVariants({variant, size, className})} {...props}>
+            <Text variant="body-sm-bold" className={badgeTextVariants({variant})}>
+                {children}
+            </Text>
+        </div>
+    );
+}
