0f1e217d7bf53970a48a0817127e8f4d184a65ee
Lucas Guedes <lucas.gkrt@gmail.com>
2025-08-26 17:14:22 -0300

    feat: Desenvolve componente Text para gerenciamento de tipografia
    Este commit introduz um componente `Text` reutilizável para padronizar a tipografia em toda a aplicação.
    
    - Utiliza a biblioteca `cva` para definir e aplicar diferentes variantes de texto, como "body-sm-bold" e "body-md-bold".
    - O componente permite renderizar diferentes elementos HTML (ex: `<span>`, `<p>`, `<h1>`) por meio da prop `as`, garantindo flexibilidade.
    - O uso de `cva` centraliza os estilos, o que simplifica a manutenção e garante uma aparência consistente para o texto.
    - O componente foi projetado para ser modular e fácil de usar em outros componentes, como o `Button` e `Badge`.

--- src/components/text.tsx
+++ src/components/text.tsx
@@ -1,16 +1,30 @@
 import React from "react";
+import {cva, type VariantProps} from "class-variance-authority";
 
-interface TextProps {
+export const textVariants = cva("font-sans text-gray-400", {
+    variants: {
+        variant:{
+            "body-sm-bold": "text-sm leading-5 font-semibold",
+            "body-md": "text-base leading-6 font-regular",
+            "body-md-bold": "text-base leading-6 font-semibold"
+        }
+    },
+    defaultVariants: {
+        variant: "body-md"
+    }
+});
+
+interface TextProps extends VariantProps<typeof textVariants>{
     as?: keyof React.JSX.IntrinsicElements;
     className?: string;
     children: React.ReactNode;
 }
 
-export default function Text({as = "span", className, children, ...props}: TextProps) {
+export default function Text({as = "span", variant, className, children, ...props}: TextProps) {
     return React.createElement(
         as, 
         {
-            className,
+            className: textVariants({variant, className}),
             ...props
         },
         children
