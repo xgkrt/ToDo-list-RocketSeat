33a57cec401f5605ab61c59349b49f36305e1276
Lucas Guedes <lucas.gkrt@gmail.com>
2025-08-27 10:52:25 -0300

    feat: Desenvolve componente InputText para entrada de texto
    Este commit introduz um componente `InputText` estilizado, projetado para entradas de texto no projeto.
    
    - O componente é criado para ser **reutilizável**, o que garante a consistência visual em todos os formulários.
    - Utiliza a biblioteca `cva` para gerenciar as classes de estilo, incluindo estados como `focus` e `disabled`.
    - O estilo base do componente `Text` é aplicado para garantir a consistência da **tipografia**.
    - O código é bem tipado, o que aumenta a segurança e a manutenibilidade do projeto.

---
+++ src/components/input-text.tsx
@@ -0,0 +1,39 @@
+import { cva, cx, type VariantProps } from "class-variance-authority";
+import { textVariants } from "./text";
+
+export const inputTextVariants = cva(`
+    border-b border-solid border-gray-200 focus:border-pink-base
+    bg-transparent outline-none
+`,{
+    variants:{
+        size:{
+            md: "pb-2 px-2"
+        },
+        disabled:{
+            true: "pointer-events-none"
+        }
+    },
+    defaultVariants:{
+        size: "md",
+        disabled: false,
+    }
+});
+
+interface InputTextProps extends VariantProps<typeof inputTextVariants>,
+    Omit<React.ComponentProps<"input">, "size" | "disabled"> {}
+
+export default function InputText({
+    size,
+    disabled,
+    className,
+    ...props
+}: InputTextProps){
+    return (
+        <input
+            className={cx(inputTextVariants({size, disabled}),
+            textVariants(),
+            className
+        )}
+        />
+    )
+}
