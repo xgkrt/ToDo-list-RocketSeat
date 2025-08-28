ab4814deab423cdb273d13ac9feaab040262536b
Lucas Guedes <lucas.gkrt@gmail.com>
2025-08-28 17:23:12 -0300

    feat: Implementa a lógica de manipulação de tarefas no hook useTask
    Este commit refatora o hook `useTask` para adicionar as funcionalidades de criação, atualização e exclusão de tarefas (CRUD).
    
    - **`prepareTask`:** Adiciona uma nova tarefa com um `id` único e um estado `Creating`, o que é útil para a experiência do usuário.
    - **`updateTask`:** Permite atualizar o título de uma tarefa existente e muda seu estado para `Created`. A função simula uma operação assíncrona com um `delay`.
    - **`updateTaskStatus`:** Lida com a atualização do status de conclusão de uma tarefa.
    - **`deleteTask`:** Remove uma tarefa da lista com base em seu `id` e também simula uma operação assíncrona.
    - **Estados de carregamento:** Adiciona os estados `isUpdatingTask` e `isDeletingTask` para exibir feedback visual ao usuário durante as operações.

--- src/hooks/use-task.ts
+++ src/hooks/use-task.ts
@@ -1,8 +1,12 @@
 import useLocalStorage from "use-local-storage";
 import { TASKS_KEY, TaskState, type Task } from "../models/task";
+import { delay } from "../helpers/utils";
+import React from "react";
 
 export default function useTask(){
     const [tasks, setTasks] = useLocalStorage<Task[]>(TASKS_KEY, []);
+    const [isUpdatingTask, setIsUpdatingTask] = React.useState(false);
+    const [isDeletingTask, setIsDeletingTask] = React.useState(false);
 
     function prepareTask(){
         setTasks([
@@ -15,10 +19,15 @@ export default function useTask(){
         ]);
     }
 
-    function updateTask(id: string, payload: {title: Task["title"]}){
+    async function updateTask(id: string, payload: {title: Task["title"]}){
+        setIsUpdatingTask(true);
+        await delay(1000);
+
         setTasks(
             tasks.map((task) => task.id === id? {...task, state: TaskState.Created, ...payload} : task)
         )
+
+        setIsUpdatingTask(false);
     }
 
     function updateTaskStatus(id: string, concluded: boolean){
@@ -27,14 +36,18 @@ export default function useTask(){
         )
     }
 
-    function deleteTask(id: string){
+    async function deleteTask(id: string){
+        setIsDeletingTask(true);
         setTasks(tasks.filter((tasks) => tasks.id != id));
+        setIsDeletingTask(false);
     }
 
     return{
         updateTask,
         prepareTask,
         updateTaskStatus,
-        deleteTask
+        deleteTask,
+        isUpdatingTask,
+        isDeletingTask
     };
 }
