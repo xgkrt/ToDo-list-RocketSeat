abe3271761f6b0a636384f6bbcd0bc9ddcd2687d
Lucas Guedes <lucas.gkrt@gmail.com>
2025-08-28 15:26:44 -0300

    feat: adicionando manipulação
    - retirando taskItem fixas e colocando com interatividade com o localStorage

--- src/core-components/tasks-list.tsx
+++ src/core-components/tasks-list.tsx
@@ -1,20 +1,36 @@
 import Button from "../components/button";
 import PlusIcon from '../assets/icons/Plus-Regular.svg?react';
 import TaskItem from "./task-item";
+import useTasks from "../hooks/use-tasks";
+import useTask from "../hooks/use-task";
+import { TaskState } from "../models/task";
 
 
 
 export default function TasksList() {
+    const { tasks } = useTasks();
+    const { prepareTask } = useTask();
+
+    function handleNewTask() {
+        prepareTask();
+    }
+
     return (
         <>
             <section>
-                <Button icon={PlusIcon} className="w-full">Nova tarefa</Button>
+                <Button
+                    icon={PlusIcon}
+                    className="w-full"
+                    onClick={handleNewTask}
+                    disabled={tasks.some((task) => task.state === TaskState.Creating)}
+                >
+                    Nova tarefa
+                </Button>
             </section>
             <section className="space-y-2">
-                <TaskItem />
-                <TaskItem />
-                <TaskItem />
-                <TaskItem />
+                {tasks.map((task) => (
+                    <TaskItem key={task.id} task={task} />
+                ))}
             </section>
         </>
     )
