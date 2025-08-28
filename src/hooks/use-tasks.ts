7699f83aff871d227f56d8b6aa9ef8867aa35a36
Lucas Guedes <lucas.gkrt@gmail.com>
2025-08-28 16:52:23 -0300

    feat: Adiciona hook customizado para gestão de tarefas
    Este commit refatora a lógica de gerenciamento de tarefas, encapsulando o estado e os efeitos colaterais em um hook customizado.
    
    - **Centralização de Lógica:** Cria o hook `useTasks` para centralizar o estado (`tasks`, `isLoadingTasks`) e as funções de manipulação de dados, o que evita a duplicação de código.
    - **Simulação de Carregamento:** O hook simula uma busca assíncrona, útil para testar estados de carregamento da interface.
    - **Sincronização de Estado:** O `useEffect` é configurado para sincronizar o estado da lista de tarefas sempre que houver uma mudança no `localStorage`, garantindo que a UI esteja sempre atualizada.
    - **Dados Derivados:** O hook também calcula e retorna contadores para as tarefas criadas e concluídas, facilitando o uso dessas informações em outros componentes.

--- src/hooks/use-tasks.ts
+++ src/hooks/use-tasks.ts
@@ -1,12 +1,30 @@
 import useLocalStorage from "use-local-storage";
 import { TASKS_KEY, TaskState, type Task } from "../models/task";
+import React from "react";
+import { delay } from "../helpers/utils";
 
 export default function useTasks(){
-    const [tasks, setTasks] = useLocalStorage<Task[]>(TASKS_KEY, []);
+    const [taskData] = useLocalStorage<Task[]>(TASKS_KEY, []);
+    const [tasks, setTasks] = React.useState<Task[]>([]);
+    const [isLoadingTasks, setIsLoadingTasks] = React.useState(true);
+
+    async function fetchTasks(){
+        if(isLoadingTasks){
+            await delay(2000);
+            setIsLoadingTasks(false);
+        }
+
+        setTasks(taskData);
+    }
+
+    React.useEffect(() => {
+        fetchTasks();
+    }, [taskData]);
 
     return{
         tasks,
-        tasksCount: tasks.length,
-        concludedTasksCount: tasks.filter((task) => task.concluded).length
+        createdTasksCount: tasks.filter((task) => task.state === TaskState.Created).length,
+        concludedTasksCount: tasks.filter((task) => task.concluded).length,
+        isLoadingTasks
     }
 }
