204a258e5bf9def4dd5ad31820454ce1d1827baa
Lucas Guedes <lucas.gkrt@gmail.com>
2025-08-28 15:27:49 -0300

    feat: interatividade com os botões
    -manipulando botões para ter suas proprias funções

--- src/core-components/task-item.tsx
+++ src/core-components/task-item.tsx
@@ -8,45 +8,102 @@ import CheckIcon from '../assets/icons/Check-Regular.svg?react';
 import XIcon from '../assets/icons/X-Regular.svg?react';
 import React from "react";
 import InputText from "../components/input-text";
+import { TaskState, type Task } from "../models/task";
+import { cx } from "class-variance-authority";
+import useTask from "../hooks/use-task";
 
+interface TaskItemProps {
+    task: Task;
+}
 
-export default function TaskItem() {
-    const [isEditing, setIsEditing] = React.useState(false)
+export default function TaskItem({ task }: TaskItemProps) {
+    const [isEditing, setIsEditing] = React.useState(task?.state === TaskState.Creating)
+
+    const [taskTitle, setTaskTitle] = React.useState(task.title || "");
+    const { updateTask, updateTaskStatus, deleteTask } = useTask();
 
     function handleEditTask() {
         setIsEditing(true);
     }
 
     function handleExistTask() {
+        if(task.state === TaskState.Creating){
+            deleteTask(task.id);
+        }
         setIsEditing(false);
     }
 
+    function handleChangeTaskTitle(e: React.ChangeEvent<HTMLInputElement>) {
+        setTaskTitle(e.target.value || "")
+    }
+
+    function handleSaveTask(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        updateTask(task.id, { title: taskTitle })
+        setIsEditing(false);
+    }
+
+    function handleChangeTaskStatus(e: React.ChangeEvent<HTMLInputElement>) {
+        const checked = e.target.checked;
+
+        updateTaskStatus(task.id, checked);
+    }
+
+    function handleCLickDeleteTask() {
+        deleteTask(task.id)
+    }
+
     return (
-        <Card size="md" className="flex items-center gap-4">
+        <Card size="md" >
             {!isEditing ? (
-                <>
-                    <InputCheckbox />
-                    <Text className="flex-1">Fazer compras da semana</Text>
+                <div className="flex items-center gap-4">
+                    <InputCheckbox
+                        value={task?.concluded?.toString()}
+                        checked={task?.concluded}
+                        onChange={handleChangeTaskStatus}
+                    />
+                    <Text className={cx("flex-1", {
+                        "line-through": task?.concluded,
+                    })}
+                    >
+                        {task?.title}
+                    </Text>
                     <div className="flex gap-1">
-                        <ButtonIcon icon={TrashIcon} variant="tertiary" />
+                        <ButtonIcon
+                            icon={TrashIcon}
+                            variant="tertiary"
+                            onClick={handleCLickDeleteTask}
+                        />
                         <ButtonIcon
                             icon={PencilIcon}
                             variant="tertiary"
                             onClick={handleEditTask}
                         />
                     </div>
-                </>
+                </div>
             ) : (
-                <>
-                    <InputText className="flex-1" />
+                <form onSubmit={handleSaveTask} className="flex items-center gap-4">
+                    <InputText
+                        value={taskTitle}
+                        className="flex-1"
+                        onChange={handleChangeTaskTitle}
+                        required
+                        autoFocus
+                    />
                     <div className="flex gap-1">
-                        <ButtonIcon icon={XIcon} variant="secondary" onClick={handleExistTask}/>
                         <ButtonIcon
+                            type="button"
+                            icon={XIcon}
+                            variant="secondary"
+                            onClick={handleExistTask}
+                        />
+                        <ButtonIcon
+                            type="submit"
                             icon={CheckIcon}
                             variant="primary"
                         />
                     </div>
-                </>
+                </form>
             )}
 
         </Card>
